5c2df5575dbf5cbfcf40ed5b04af7ea0db116819
zakariem <zakariyem367@gmail.com>
2024-11-27 06:50:48 +0300

    Add SVG assets and update pubspec.yaml
    Added multiple SVG assets for various icons and updated the pubspec.yaml file to include the new SVG assets directory.

---
+++ lib/constants/svgs/svgs.dart
@@ -0,0 +1,37 @@
+// ignore_for_file: prefer_single_quotes
+class Svgs {
+  Svgs._();
+
+  static const String resourceSvgsComment = "assets/svgs/comment.svg";
+
+  static const String resourceSvgsEmoji = "assets/svgs/emoji.svg";
+
+  static const String resourceSvgsGallery = "assets/svgs/gallery.svg";
+
+  static const String resourceSvgsGif = "assets/svgs/gif.svg";
+
+  static const String resourceSvgsHomeFilled = "assets/svgs/home_filled.svg";
+
+  static const String resourceSvgsHomeOutlined =
+      "assets/svgs/home_outlined.svg";
+
+  static const String resourceSvgsLikeFilled = "assets/svgs/like_filled.svg";
+
+  static const String resourceSvgsLikeOutlined =
+      "assets/svgs/like_outlined.svg";
+
+  static const String resourceSvgsNotifFilled = "assets/svgs/notif_filled.svg";
+
+  static const String resourceSvgsNotifOutlined =
+      "assets/svgs/notif_outlined.svg";
+
+  static const String resourceSvgsRetweet = "assets/svgs/retweet.svg";
+
+  static const String resourceSvgsSearch = "assets/svgs/search.svg";
+
+  static const String resourceSvgsTwitterLogo = "assets/svgs/twitter_logo.svg";
+
+  static const String resourceSvgsVerified = "assets/svgs/verified.svg";
+
+  static const String resourceSvgsViews = "assets/svgs/views.svg";
+}
